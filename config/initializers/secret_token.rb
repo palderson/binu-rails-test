cb6489cc186ddbf2e8897db4ee3d4f85c38c0377
Michael Benedict <thebenedict@gmail.com>
2012-09-29 00:18:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BinuTest::Application.config.secret_token = '8a8007dfa4064e84252fb43589d13cf6d4aba8ba8a809ade3a4cc63defccf8766a33dc3726df6a989e71bb5b2f2f43d685b9fd43bc0cb9166a17a2fe85d42570'
